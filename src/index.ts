65138050da20123abef5339ee33108da155272af
Jwata <watanabe0621@gmail.com>
2020-04-02 01:21:44 +0900

    import lib in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as _ from 'lodash';
 
+import { split, reconstruct } from './lib/shamir_secret_sharing';
+
 function component(): HTMLElement {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'MPC', 'ECDSA'], ' ');
+  element.innerHTML = _.join(
+    ['Hello', 'MPC', 'ECDSA', reconstruct(split(1, 3, 2), 3, 2)], ' ', );
 
   return element;
 }
