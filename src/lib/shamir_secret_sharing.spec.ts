ee552c5d98a089e8eec182c9ba857c7bd098684b
Jwata <watanabe0621@gmail.com>
2020-04-03 09:19:54 +0900

    add test for reconstruct

--- src/lib/shamir_secret_sharing.spec.ts
+++ src/lib/shamir_secret_sharing.spec.ts
@@ -1,8 +1,19 @@
-import { split } from './shamir_secret_sharing';
+import { split, reconstruct } from './shamir_secret_sharing';
+
+const N = 3; // number of party
+const K = 2; // threshold
 
 describe('split', function() {
-  it('generates shares', function(){
-    const shares = split(1, 3, 2);
-    expect(shares.length).toEqual(3);
+  it('generates shares from secret', function(){
+    const shares = split(1, N, K);
+    expect(shares.length).toEqual(N);
+  });
+});
+
+describe('reconstruct', function() {
+  it('reconstructs secret from shares', function(){
+    const secret = 1;
+    const actual = reconstruct(split(secret, N, K), N, K);
+    expect(actual).toEqual(secret);
   });
 });
